87dd01562bdebbc91ec8127aa1e6abb3fbd164e7
nazirov_istam <inazirov.unlimit@gmail.com>
2025-03-02 11:36:43 +0500

    init commit

---
+++ src/test/java/com/example/leetcode_solutions/LeetcodeSolutionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.leetcode_solutions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeetcodeSolutionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
