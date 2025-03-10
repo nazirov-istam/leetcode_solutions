b615a0901a058171a52e14465b39a6dd4dd48962
nazirov_istam <inazirov.unlimit@gmail.com>
2025-03-10 14:50:26 +0500

    longestPalindrome

---
+++ src/main/java/com/example/leetcode_solutions/string/problem5.java
@@ -0,0 +1,38 @@
+package com.example.leetcode_solutions.string;
+
+public class problem5 {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome("aab"));
+    }
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.isEmpty()) {
+            return "";
+        }
+
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int odd = expandAroundCenter(s, i, i);
+            int even = expandAroundCenter(s, i, i + 1);
+            int max_len = Math.max(odd, even);
+
+            if (max_len > end - start) {
+                start = i - (max_len - 1) / 2;
+                end = i + max_len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
+
